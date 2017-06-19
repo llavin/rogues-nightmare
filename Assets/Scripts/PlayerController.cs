b5f8e25e7f6df4c714ffabb9bbdf98bafa04929a
llavin <llavin91@gmail.com>
2017-06-19 18:19:43 -0500

    First commit, branching off of 2d tutorial. Added custom 2d player controller

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float maxSpeed = 3f;
+    private bool facingRight = true;
+
+    //GROUNDED JUMPY STUFF
+    bool grounded = false;
+    public Transform groundCheck;
+    float groundRadius = 0.2f;
+    public LayerMask whatIsGround;
+    public float jumpForce = 700;
+    bool doubleJump = false;
+
+    Rigidbody2D rb2d;
+    Animator anim;
+
+	// Use this for initialization
+	void Start ()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate ()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheck.position, groundRadius, whatIsGround);
+
+        if (grounded)
+            doubleJump = false;
+        
+        anim.SetBool("Ground", grounded);
+        anim.SetFloat("vSpeed", rb2d.velocity.y);
+
+        float move = Input.GetAxis("Horizontal");
+
+        //TODO
+        //Parameter for animator to include running animation
+        //anim.SetFloat("Speed", Mathf.Abs(move));
+
+        rb2d.velocity = new Vector2(move * maxSpeed, rb2d.velocity.y);
+
+        if (move > 0 && !facingRight)
+            Flip();
+        else if (move < 0 && facingRight)
+            Flip();        
+	}
+
+    private void Update()
+    {
+        if ((grounded || !doubleJump) && Input.GetButtonDown("Jump"))
+        {
+            anim.SetBool("Ground", false);
+            rb2d.AddForce(new Vector2(0, jumpForce));
+
+            if (!doubleJump && !grounded)
+                doubleJump = true;
+        }
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
